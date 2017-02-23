9508312b7038676b316653b63f0aa01cc5ed328a
TristonJ <triston@rolltechbowling.com>
2017-02-23 12:56:32 -0800

    Added the prefer arrow functions and the unit tests for it

---
+++ test/lib/rules/prefer-arrow-functions.js
@@ -0,0 +1,32 @@
+/**
+ * @fileoverview Tests for prefer-arrow-functions rule.
+ * @author Triston Jones
+ */
+
+'use strict';
+
+var rule = require('../../../lib/rules/prefer-arrow-functions'),
+    RuleTester = require('eslint').RuleTester;
+
+var tester = new RuleTester({parserOptions: {ecmaVersion: 6}});
+tester.run('lib/rules/prefer-arrow-functions', rule, {
+  parserOptions: {ecmaVersion: 6},
+  valid: [
+    'var foo = (bar) => bar;',
+    'var foo = bar => bar;',
+    'var foo = bar => { return bar; }',
+    'var foo = () => 1;',
+    'var foo = (bar, fuzz) => bar + fuzz',
+    '["Hello", "World"].reduce((p, a) => p + " " + a);',
+    'var foo = (...args) => args',
+    'var obj = {constructor: ()=>{}}; obj.prototype.func = function() {};',
+    'var obj = {constructor: ()=>{}}; obj.prototype = {func: function() {}};'
+  ],
+  invalid: [
+    {code: 'function foo() { return "Hello!"; }', errors: ['Prefer using arrow functions over plain functions']},
+    {code: 'function foo() { return arguments; }', errors: ['Prefer using arrow functions over plain functions']},
+    {code: 'var foo = function() { return "World"; }', errors: ['Prefer using arrow functions over plain functions']},
+    {code: '["Hello", "World"].reduce(function(a, b) { return a + " " + b; })', errors: ['Prefer using arrow functions over plain functions']},
+    {code: 'var obj = {constructor: ()=>{}}; obj.prototype.func = function() {};', errors: ['Prefer using arrow functions over plain functions'], options: [{disallowPrototype:true}]}
+  ]
+});
