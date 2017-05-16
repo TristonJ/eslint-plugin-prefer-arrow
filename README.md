5be9b872e717336772b7c7423a9878ddbf600e6c
Keith Lea <keithl@uber.com>
2017-05-16 15:20:43 -0400

    Initial commit of `singleReturnOnly` option

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # eslint-plugin-prefer-arrow
-ESLint plugin to prefer arrow functions. By default, the plugin allows usage of `function` as a member of an Object's prototype, but this can be changed with the property `disallowPrototype`.
+ESLint plugin to prefer arrow functions. By default, the plugin allows usage of `function` as a member of an Object's prototype, but this can be changed with the property `disallowPrototype`. Alternatively, with the `singleReturnOnly` option, this plugin only reports functions where converting to an arrow function would dramatically simplify the code.
 
 # Installation
 
@@ -21,10 +21,13 @@ Add the plugin to the `plugins` section and the rule to the `rules` section in y
   "prefer-arrow/prefer-arrow-functions": [
      "warn",
      {
-       "disallowPrototype": true
+       "disallowPrototype": true,
+       "singleReturnOnly": false
      }
    ]
 ]
 ```
 # Configuration
-There is currently only one configuration option, `disallowPrototype`. If set to true, the plugin will warn if `function` is used anytime. Otherwise, the plugin allows usage of `function` if it is a member of an Object's prototype.
+ * `disallowPrototype`: If set to true, the plugin will warn if `function` is used anytime. Otherwise, the plugin allows usage of `function` if it is a member of an Object's prototype.
+ * `singleReturnOnly`: If set to true, the plugin will only warn for `function` declarations which *only* contain a return statement. These often look much better when declared as arrow functions without braces. Works well in conjunction with ESLint's built-in [arrow-body-style](http://eslint.org/docs/rules/arrow-body-style) set to `as-needed`.
+ 
--- test/lib/rules/prefer-arrow-functions.js
+++ test/lib/rules/prefer-arrow-functions.js
@@ -5,6 +5,8 @@
 
 'use strict';
 
+const singleReturnOnly = code => ({code, options: [{singleReturnOnly: true}]});
+
 var rule = require('../../../lib/rules/prefer-arrow-functions'),
     RuleTester = require('eslint').RuleTester;
 
@@ -20,13 +22,30 @@ tester.run('lib/rules/prefer-arrow-functions', rule, {
     '["Hello", "World"].reduce((p, a) => p + " " + a);',
     'var foo = (...args) => args',
     'class obj {constructor(foo){this.foo = foo;}}; obj.prototype.func = function() {};',
-    'class obj {constructor(foo){this.foo = foo;}}; obj.prototype = {func: function() {}};'
+    'class obj {constructor(foo){this.foo = foo;}}; obj.prototype = {func: function() {}};',
+    ...[
+        'var foo = (bar) => {return bar();}',
+        'function foo(bar) {bar()}',
+        'var x = function foo(bar) {bar()}',
+        'var x = function(bar) {bar()}',
+        'function foo(bar) {/* yo */ bar()}',
+        'function foo() {}',
+        'function foo(bar) {bar(); return bar()}',
+    ].map(singleReturnOnly)
   ],
   invalid: [
     {code: 'function foo() { return "Hello!"; }', errors: ['Use const or class constructors instead of named functions']},
     {code: 'function foo() { return arguments; }', errors: ['Use const or class constructors instead of named functions']},
     {code: 'var foo = function() { return "World"; }', errors: ['Prefer using arrow functions over plain functions']},
     {code: '["Hello", "World"].reduce(function(a, b) { return a + " " + b; })', errors: ['Prefer using arrow functions over plain functions']},
-    {code: 'class obj {constructor(foo){this.foo = foo;}}; obj.prototype.func = function() {};', errors: ['Prefer using arrow functions over plain functions'], options: [{disallowPrototype:true}]}
+    {code: 'class obj {constructor(foo){this.foo = foo;}}; obj.prototype.func = function() {};', errors: ['Prefer using arrow functions over plain functions'], options: [{disallowPrototype:true}]},
+    ...[
+        'function foo() { return 3; }',
+        'function foo(a) { return 3; }',
+        'var foo = function() { return "World"; }',
+        'var foo = function x() { return "World"; }',
+        'function foo(a) { /* yo */ return 3; }',
+        'function foo(a) { return a && (3 + a()) ? true : 99; }',
+    ].map(singleReturnOnly).map(testCase => Object.assign({errors: ['Prefer using arrow functions over plain functions which only return a value']}, testCase))
   ]
 });
